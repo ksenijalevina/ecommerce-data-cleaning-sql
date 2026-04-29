2f11d1a4447dbd8338089a9262ba72a715a312d2
ksenijalevina <ksenijalevina08@gmail.com>
2026-04-29 19:02:11 +0300

    Add project description and status
    Refactor SQL script for data cleaning and standardization in ecommerce sales data. Includes creating new tables, removing duplicates, and standardizing data formats.

--- ecommerce_s.sql
+++ ecommerce_s.sql
@@ -9,36 +9,56 @@ FROM ecommerce_sales.messy_ecommerce_sales_data;
 -- 2. Standardize the Data
 -- 3. Null values and blank values
 -- 4. Remove Any Columns
+-- Data Cleaning 
+SELECT * FROM messy_ecommerce_sales_data;
 
-CREATE TABLE ecommerce_sales
-LIKE ecommerce_sales.messy_ecommerce_sales_data;
+# make a copy of the dataset
 
-SELECT * 
-FROM ecommerce_sales; #blogas formatas, netinkamas total stulpelis
+create table e_com 
+like messy_ecommerce_sales_data;
 
-INSERT ecommerce_sales 
-SELECT * FROM 
-ecommerce_sales.messy_ecommerce_sales_data;
+insert e_com
+select * from messy_ecommerce_sales_data;
+
+select * from e_com;
+
+#remove duplicates
 
 SELECT *,
 ROW_NUMBER() OVER(
-PARTITION BY Order_ID, Order_Date, Product) AS row_num
-FROM  ecommerce_sales;
+PARTITION BY Customer_name, Order_ID, Order_Date, Product, category, quantity, price, total) AS row_num
+FROM  e_com;
 
- -- DUPLICATES
-WITH duplicate_s AS
-(
+
+
+
+
+
+select * from e_com
+where customer_name = 'Customer_146'; 
+
+# different total, maybe it was some discount??? 
+
+select * from e_com
+where customer_name = 'Customer_142'; 
+
+select * from e_com
+where customer_name = 'Customer_175'; 
+
+## 
+
+select * from e_com;
+insert into e_com
+
+
 SELECT *,
 ROW_NUMBER() OVER(
-PARTITION BY id, customer_name, order_id, order_date, product,
-                 category, quantity, price, payment_method, status, total) AS row_num
-FROM  ecommerce_sales
-)
-SELECT * 
-FROM duplicate_s
-WHERE row_num > 1;
+PARTITION BY Customer_name, Order_ID, Order_Date, Product, category, quantity, price, total) AS row_num
+FROM  e_com;
 
-CREATE TABLE `ecommerce_sales2` (
+#create a new table for deleting duplicates
+
+CREATE TABLE `e_com2` (
   `ID` int DEFAULT NULL,
   `Customer_Name` text,
   `Order_ID` text,
@@ -53,101 +73,125 @@ CREATE TABLE `ecommerce_sales2` (
   `row_num` int
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-SELECT *
-FROM ecommerce_sales2;
 
-INSERT INTO ecommerce_sales2
+
+insert into e_com2
 SELECT *,
 ROW_NUMBER() OVER(
 PARTITION BY id, customer_name, order_id, order_date, product,
                  category, quantity, price, payment_method, status, total) AS row_num
-FROM  ecommerce_sales;
-SELECT *
-FROM ecommerce_sales2
-WHERE row_num > 1;
+FROM  e_com;
 
+select * from e_com2
+where row_num > 1;
 
+#checking
+select * from e_com2
+where ID = 146;
 
-DELETE
-FROM ecommerce_sales2
-WHERE row_num > 1;
+#duplicated rows is deleted
 
-SELECT *
-FROM ecommerce_sales2;
+delete from e_com2
+where row_num > 1;
 
--- Standardizing data
+#delee column row_num
 
-SELECT Category
-from ecommerce_sales2
-order by 1;
-select distinct Category
-from ecommerce_sales2;
+alter table e_com2
+drop column row_num;
 
-SELECT LOWER(Category)
-FROM ecommerce_sales2
-ORDER BY 1;
+select * from e_com2;
 
-UPDATE ecommerce_sales2
-SET category = LOWER(Category);
+select ID from e_com2;
 
-
-select distinct category
-from ecommerce_sales2;
-
-select distinct category
-from ecommerce_sales2;
-
+WITH duplicate_s AS
+(
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY id) AS row_num
+FROM  ecommerce_sales
+)
 SELECT * 
-FROM ecommerce_sales2
-WHERE Category LIKE 'electronic%'; 
+FROM duplicate_s
+WHERE row_num > 1;
 
-UPDATE ecommerce_sales2 
-SET Category = 'electronics'
+
+select * from e_com2
+where id in(142,146,175);  #we can notice totals differneces (maybe it was discount)
+
+#lets get move on standardize 
+
+select customer_name from e_com2;
+
+
+#standardize a date column
+
+alter table e_com2 
+add column order_date2 text;
+
+
+update e_com2
+set order_date2 = case
+    when order_date like '%/%' then str_to_date(order_date, '%m/%d/%Y')
+    else str_to_date(order_date, '%b %e %Y')
+end;
+
+
+alter table e_com2
+drop column order_date;
+
+alter table e_com2
+change order_date2 Order_date date;
+
+# product column
+select distinct product from e_com2;
+
+alter table e_com2 
+modify column product text after order_date;
+# quantity column
+select Quantity from e_com2;
+
+alter table e_com2
+add column quan int;
+
+update e_com2
+set quan = case
+	when quantity < 0 then abs(quantity)
+    else 
+		quantity
+end;
+
+alter table e_com2
+drop column quantity;
+
+alter table e_com2
+change quan Quantity int;
+
+select category from e_com2;
+
+select * from e_com2;
+
+#column category
+select distinct category from e_com2;
+
+update e_com2 
+set category = lower(category);
+
+UPDATE e_com2 
+SET category = 'electronics'
 WHERE category LIKE 'electronic%';
 
-select distinct category
-from ecommerce_sales2;
+UPDATE e_com2 
+SET category = null
+WHERE category in('nan', '');
 
-SELECT *
-FROM ecommerce_sales2;
+alter table e_com2 
+modify column category text after quantity;
 
--- DELETING THE COLUMN
-ALTER TABLE ecommerce_sales2
-DROP COLUMN row_num;
+#column price
 
-select category
-from ecommerce_sales2
-ORDER BY 1;
+select price from e_com2;
 
-SELECT DISTINCT Status
-FROM ecommerce_sales2
-ORDER BY 1;
-
-select order_date
-from ecommerce_sales2
-ORDER BY 1;
-
-SELECT 'Order_date'
-STR_TO_DATE('Order_date', '%m/%d/%Y')
-FROM ecommerce_sales2
-
-
-
-select order_date,
-case
-    when order_date like '%/%' then str_to_date(order_date, '%m/%d/%Y')
-    else str_to_date(order_date, '%b %e %Y')
-end as cleaned_date
-from ecommerce_sales2;
-
-update ecommerce_sales2
-set order_date = case
-    when order_date like '%/%' then str_to_date(order_date, '%m/%d/%Y')
-    else str_to_date(order_date, '%b %e %Y')
-end as cleaned_date;
-
--- create a new column
-
+select * from e_com2;
 alter table ecommerce_sales2
 add column cl
 
